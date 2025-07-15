24a6b2e370b4b61f1443aa9d5bbd932deee3ef8b
zainmalik-invo <zain.abdin@invozone.dev>
2025-07-15 18:51:24 +0500

    replay token change

--- test/longpolling_subscription_test.exs
+++ test/longpolling_subscription_test.exs
@@ -73,7 +73,7 @@ defmodule RigInboundGateway.LongpollingSubscriptionTest do
   end
 
   defp connection_id(welcome_event)
-  defp connection_id(%{"data" => %{"connection_token" => connection_id}}), do: connection_id
+  defp connection_id(%{"data" => %{"replay_token" => connection_id}}), do: connection_id
 
   describe "Connections and subscriptions:" do
     test "Connecting with no parameters causes empty subscription set." do
